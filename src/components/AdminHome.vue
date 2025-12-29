ab596fa69ba58c599e6f5f4b51b1e41e9a87cd00
303729223@qq.com <yong6457493>
2025-12-29 11:21:49 +0800

    新增教培圈app构建地址

--- src/components/AdminHome.vue
+++ src/components/AdminHome.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="admin-home">
-    <h2>jenkins构建地址</h2>
-    <p>请<a href="http://8.129.86.105:8887/" target="_blank">点击此处在新窗口打开 Jenkins 构建地址</a>。</p>
+    <h2>接口自动化工程构建地址</h2>
+    <p><a href="http://8.129.86.105:8887/" target="_blank">接口自动化工程构建地址</a>。</p>
+    <hr>
+    <h2>教培圈app 构建地址</h2>
+    <p><a href="http://192.168.1.157:9880/" target="_blank">教培圈app 构建地址</a>。</p>
   </div>
 </template>
 <script>
@@ -12,6 +15,11 @@ export default {
 <style scoped>
 .admin-home {
   padding: 12px;
+}
+.admin-home hr {
+  border: none;
+  border-top: 1px solid #ccc;
+  margin: 20px 0;
 }
 .error {
   color: red;
